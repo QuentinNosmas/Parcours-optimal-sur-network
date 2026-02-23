d2aa66cb9c574db40f07a68de46ad18aba97c0c6
quentinnosmas <quentin.nosmas@ensae.fr>
2026-02-23 08:01:02 +0000

    push test

--- code/graph.py
+++ code/graph.py
@@ -2,6 +2,10 @@
 This is the graph module. It contains the classes Graph and GraphImplicit
 """
 import heapq
+<<<<<<< Updated upstream
+=======
+import os
+>>>>>>> Stashed changes
 
 
 class Graph:
@@ -49,6 +53,31 @@ class Graph:
                     heapq.heappush(P, (d[u], u))
 
         return d,predecesseurs
+
+
+    #Le graphe étant à poids positifs on peut implémenter l'algorithme de Djikstra pour
+    #trouver le chemin de longueur minimal 
+
+    def shortest_path(self,start_node):
+        P=[(0,start_node)]
+        nodes = list(self._edges.keys())
+        d = {node : float('inf') for node in nodes}
+        d[start_node] = 0
+        predecesseurs = {node: None for node in nodes}
+        while len(P)!=0 :
+            dist_a, u_actuel = heapq.heappop(P)
+
+            if dist_a > d[u_actuel]:
+                continue
+
+            for u , weight in self.neighbours( u_actuel ):
+                if d[u]>d[u_actuel]+weight:
+                    d[u]=d[u_actuel]+weight
+                    predecesseurs[u] = u_actuel
+                    heapq.heappush(P, (d[u], u))
+
+        return d,predecesseurs
+
 
 
 
--- code/main.py
+++ code/main.py
@@ -1,9 +1,10 @@
-from network import Network
+from network import *
 from graph import Graph
 
 # Load the network
-network_file = "../examples/small.txt"
+network_file = "examples/small.txt"
 network = Network.from_file(network_file)
 
-
+g= network.build_simple_graph()
+g.shortest_path()
 
