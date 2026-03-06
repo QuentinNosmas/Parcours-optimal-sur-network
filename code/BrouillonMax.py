4daaeb41b954f52b9d7faae48a585c182f6f5b12
Maxime <duvivier.maxime1@gmail.com>
2026-03-06 23:43:31 +0100

    build_extended_graph fini

--- code/BrouillonMax.py
+++ code/BrouillonMax.py
@@ -1,4 +1,35 @@
 from graph import Graph 
+from network import Network
+
+def build_extended_graph(g : Network) -> Graph:
+    """
+    Construit un graphe étendu à partir de g. Les sommets sont les
+    (u,F) où u est sommet de g et F est une fatigue entre 0 et une 
+    fatigue maximum f_max. Les arêtes sont les (u,F) -> (v,F+f) 
+    de poids (1+F)*poids(u->v), quand u->v a une fatigue f dans g, 
+    pour toute F telle que F+f <= f_max.
+    """
+    n = len(g._roads)
+    fatigues = [voisin[2] for sommet in g._roads for voisin in g._roads[sommet]]
+    Fmax = (n-1)*max(fatigues) if fatigues else 0   
+    edges = {}
+    
+    for sommet in g._roads:  #Ajoute tous les sommet (u,F) où u est dans g et F < Fmax + 1
+        for f in range(Fmax + 1):
+            edges[(sommet, f)] = []
+    
+    for sommet in g._roads:  #Construit les arêtes (u,F) -> (v,F+f) de poids (1+F)*poids(u->v)
+        for voisin in g._roads[sommet]:
+            F = voisin[2]
+            for f in range(Fmax + 1):
+                if f + F <= Fmax:
+                    edges[(sommet, f)].append(((voisin[0], f + F), (1 + f) * voisin[1]))
+    
+    return Graph(edges)
+        
+            
+            
+
 
 
 
