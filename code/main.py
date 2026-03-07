3599b9f500795076997b71b49f7882b1c0ba4be9
Maxime <duvivier.maxime1@gmail.com>
2026-03-08 00:28:46 +0100

    ajout de GraphImplicit dans le main

--- code/main.py
+++ code/main.py
@@ -5,6 +5,24 @@ from graph import Graph
 network_file = "examples/small.txt"
 network = Network.from_file(network_file)
 
-g= network.build_simple_graph()
-g.shortest_path()
+class GraphImplicit(Graph):
+    """
+    Cette classe se sert du network pour redéfinir
+    la méthode neighbours ensuite utilisée par shortest_path
+    """
+
+    def __init__(self, network : Network):
+        self.network = network
+        n = len(network._roads)
+        fatigues = [voisin[2] for sommet in network._roads for voisin in network._roads[sommet]]
+        self.Fmax = (n-1)*max(fatigues) if fatigues else 0
+    
+    def neighbours(self, node):
+        res = []
+        sommet, f = node
+        for voisin in self.network._roads[sommet]:
+            F = voisin[2]
+            if f + F <= self.Fmax :
+                res.append(((voisin[0], f + F), (1 + f)*voisin[1]))
+        return res
 
